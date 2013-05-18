a70a70fce41edb661da1d5e8994cb7bb2bdd7760
Péter <peter@perenyi.net>
2013-05-18 17:51:32 +1200

    add comments

--- arte.sh
+++ arte.sh
@@ -1,11 +1,15 @@
 #!/bin/sh
+
+# force python unicode character encoding 
 export PYTHONIOENCODING=utf-8
 
+## create working folder
 mkdir /vagrant/dload
 cd /vagrant/dload
 
+# get the latest script
 curl https://raw.github.com/solsticedhiver/arteVIDEOS/master/arteVIDEOS.py >arteVIDEOS.py
 
-chmod +x arteVIDEOS.py 
-
+chmod +x arteVIDEOS.py
+ 
 ./arteVIDEOS.py $*
